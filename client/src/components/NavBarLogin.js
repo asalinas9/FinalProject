675335999bbf0dd6f1a2315cd4024a717ce50047
TannerMillican <tannermillican@gmail.com>
2020-07-07 19:04:16 -0500

    made navbars and footer match color

--- client/src/components/NavBarLogin.js
+++ client/src/components/NavBarLogin.js
@@ -30,7 +30,7 @@ export default function ButtonAppBar() {
 
 	return (
 		<div className={classes.root}>
-			<AppBar position="static" className={classes.bg}>
+			<AppBar position="static" >
 				<Toolbar>
 					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
 						<MenuIcon />
